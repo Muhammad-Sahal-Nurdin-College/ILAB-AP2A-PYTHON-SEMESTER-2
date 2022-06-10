3e2b4083c81ceacf6b494dff6b6d39f105b333a6
hp <sahalnurdin@gmail.com>
2022-06-10 09:59:32 +0700

    SELESAI!!!

--- Algoritma_dan_Modul.py
+++ Algoritma_dan_Modul.py
@@ -112,10 +112,115 @@ def main():
     data = [35, 73, 90, 65, 23, 86, 43, 81, 34, 58]
     data_tersortir = quicksort(data)
     print(data_tersortir)
-
 main()
 
 
+# Soal Nomor 2
+# Fungsi bantu untuk mencari indeks dari nilai minimum dalam data
+# Fungsi ini mengembalikan indeks nilai minimum dari data
+def indeks_minimum(data):
+    # [1] Tuliskan kode untuk mencari indeks nilai minimum
+    # dan kembalikan indeks tersebut.
+    indeks_min = 0
+    min1 = data[indeks_min]
+    for indeks in range(1, len(data)):
+        if data[indeks] <= min1:
+            indeks_min = indeks
+            min1 = data[indeks_min]
+    return indeks_min
 
+
+# Fungsi penyortiran seleksi secara rekursif
+def selection_sort_rekursif(data):
+    if len(data) < 2:
+        return data
+    else:
+        pivot = data[0]
+        lebih_kecil = []
+        lebih_besar = []
+        for elm in data[1:]:
+            if elm <= pivot:
+                lebih_kecil.append(elm)
+            else:
+                lebih_besar.append(elm)
+        return selection_sort_rekursif(lebih_kecil) + [pivot] + selection_sort_rekursif(lebih_besar)
+
+
+def main():
+    data = [97, 23, 45, 88, 12, 76]
+    data_tersortir = selection_sort_rekursif(data)
+
+
+main()
+
+# [2] Base Case: Data kosong atau hanya mempunyai satu elemen
+# Tidak perlu sortir, kembalikan data apa adanya
+
+# [3] Recursive Case: Data dengan dua atau lebih elemen
+# Cari indeks nilai minimum dan dapatkan nilai minimum
+# Konkatenasi list berisi nilai minimum dengan list berisi data dengan nilai minimum dihilangkan
+
+
+
+# Nomor 1
+# Fungsi sortir menerima argumen list dan
+# mengembalikan list yang tersortir
+def sortir(data):
+    # [1] Tuliskan kode algoritma penyortiran di bawah
+    # Base Cases
+    if len(data) < 2:
+        return data
+    # Recursive Cases
+    else:
+        pivot = data[0]
+        lebih_kecil = []
+        lebih_besar = []
+        # Buat list dengan elemen-elemen <= Pivot
+        # dan list dengan elemen-elemen > pivot
+        for elm in data[1:]:
+            if elm <= pivot:
+                lebih_kecil.append(elm)
+            else:
+                lebih_besar.append(elm)
+        return sortir(lebih_kecil) + [pivot] + sortir(lebih_besar)
+
+# Fungsi median menerima argumen sebuah list
+# dan mengembalikan nilai median dari data dalam list tersebut
+
+
+def median(data):
+    # [2] Gunakan fungsi sortir untuk menyortir data
+    sortir(data)
+    # [3] Hitung median dengan dua kondisi untuk jumlah data genap dan jumlah data ganjil
+    # lalu kembalikan nilai median
+    if len(data) % 2 != 0:
+        # total number of values are odd
+        # subtract 1 since indexing starts at 0
+        m = int((len(data) + 1) / 2 - 1)
+        return data[m]
+    else:
+        m1 = int(len(data) / 2 - 1)
+        m2 = int(len(data) / 2)
+        return (data[m1] + data[m2]) / 2
+
+# Module
+
+# [1] import module yang Anda tulis
+import segitigasiku
+
+
+# Fungsi main menguji module
+def main():
+
+# [2] Minta pengguna untuk memasukkan alas dan tinggi
+
+
+# [3] Hitung luas dan keliling dengan menggunakan fungsi pada module
+
+
+# [4] Tampilkan luas dan keliling dengan presisi 2 desimal
+
+
+# Panggil fungsi main
 
 
--- test.py
+++ test.py
@@ -1,2 +1,50 @@
 # test
+# Fungsi sortir menerima argumen list dan
+# mengembalikan list yang tersortir
+def sortir(data):
+    # [1] Tuliskan kode algoritma penyortiran di bawah
+    # Base Cases
+    if len(data) < 2:
+        return data
+    # Recursive Cases
+    else:
+        pivot = data[0]
+        lebih_kecil = []
+        lebih_besar = []
+        # Buat list dengan elemen-elemen <= Pivot
+        # dan list dengan elemen-elemen > pivot
+        for elm in data[1:]:
+            if elm <= pivot:
+                lebih_kecil.append(elm)
+            else:
+                lebih_besar.append(elm)
+        return sortir(lebih_kecil) + [pivot] + sortir(lebih_besar)
+
+# Fungsi median menerima argumen sebuah list
+# dan mengembalikan nilai median dari data dalam list tersebut
+
+
+def median(data):
+    # [2] Gunakan fungsi sortir untuk menyortir data
+    sortir(data)
+    # [3] Hitung median dengan dua kondisi untuk jumlah data genap dan jumlah data ganjil
+    # lalu kembalikan nilai median
+    if len(data) % 2 != 0:
+        # total number of values are odd
+        # subtract 1 since indexing starts at 0
+        m = int((len(data) + 1) / 2 - 1)
+        return data[m]
+    else:
+        m1 = int(len(data) / 2 - 1)
+        m2 = int(len(data) / 2)
+        return (data[m1] + data[m2]) / 2
+
+
+print(median([1, 3, 3, 6, 7, 8, 9]))
+print(median([1, 2, 3, 4, 5, 6, 8, 9]))
+
+
+
+
+
 
