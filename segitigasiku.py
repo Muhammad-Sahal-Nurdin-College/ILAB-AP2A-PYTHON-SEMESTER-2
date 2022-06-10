3a124b212a7867a3c228663b24fd90b4939b8ce5
hp <sahalnurdin@gmail.com>
2022-06-10 10:13:51 +0700

    SELESAI!!!

--- segitigasiku.py
+++ segitigasiku.py
@@ -5,11 +5,16 @@ import math
 
 # Fungsi luas menerima alas dan tinggi sebagai argumen
 # dan mengembalikan luas persegi panjang
+
+
 def luas(alas, tinggi):
     return (alas * tinggi)/2
+
+
 # Fungsi keliling menerima alas dan tinggi sebagai argumen
 # dan mengembalikan keliling persegi panjang
+
+
 def keliling(alas, tinggi):
     sisimiring = math.sqrt(pow(alas, 2) + pow(tinggi, 2))
-    keliling = alas + tinggi + sisimiring
-    return keliling
+    return alas + tinggi + sisimiring
