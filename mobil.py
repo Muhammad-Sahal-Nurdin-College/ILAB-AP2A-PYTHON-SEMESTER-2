3e8d297c41326d74f123e4660f92a19b5c03648a
hp <sahalnurdin@gmail.com>
2022-06-16 10:03:48 +0700

    SELESAI!!!

---
+++ mobil.py
@@ -0,0 +1,81 @@
+# Definisi class Mobil
+# Attribute: __pabrikan, __model, dan __kecepatan
+# Method: akselerasi(), rem(), get_kecepatan()
+class Mobil:
+    def __init__(self, pabrikan, model):
+        self.__pabrikan = pabrikan
+        self.__model = model
+        self.__kecepatan = 0
+
+    def akselerasi(self, __kecepatan):
+        self.__kecepatan += 5
+
+    def rem(self, __kecepatan):
+        self.__kecepatan -= 5
+        if self.__kecepatan <= 0:
+            self.__kecepatan = 'Tidak Bisa'
+        else:
+            self.__kecepatan = 0
+
+    def get_kecepatan(self, ):
+        return self.__kecepatan
+
+# [1] Tuliskan method __init__ yang menerima argumen pabrikan dan model
+# dan menugaskannya ke attribute __pabrikan dan __model.
+# Method __init__ juga menginisialisasi attribute __kecepatan dengan nilai 0
+
+
+# [2] Tuliskan method akselerasi yang menambahkan attribute __kecepatan dengan nilai 5
+
+
+# [3] Tuliskan method rem yang mengurangi attribute __kecepatan dengan nilai 5.
+# Tambahkan kondisi yang mencegah __kecepatan bernilai negatif
+
+
+# [4] Tuliskan method accessor get_kecepatan yang mengembalikan nilai attribute
+# __kecepatan
+
+def main():
+    mobil = Mobil('Toyota', 'Agya')
+
+    # Akselerasi lima kali dan tampilkan kecepatan terkini setiap kali akselerasi
+    mobil.akselerasi()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.akselerasi()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.akselerasi()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.akselerasi()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.akselerasi()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    # Rem tujuh kali dan tampilkan kecepatan terkini setiap kali rem
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+    mobil.rem()
+    print('Kecepatan:', mobil.get_kecepatan())
+
+main()
+
+
