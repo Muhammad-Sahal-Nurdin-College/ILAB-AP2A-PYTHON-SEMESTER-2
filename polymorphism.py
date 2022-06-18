291f83d94f5b4fe9e83a327d8460a13f1b00bc43
hp <sahalnurdin@gmail.com>
2022-06-18 08:12:11 +0700

    SELESAI!!!

---
+++ polymorphism.py
@@ -0,0 +1,17 @@
+# hewan.py
+# Class mamalia merepresentasikan mamalia generik
+class Mamalia:
+    # Method __init__ menerima sebuah argumen untuk    # spesies mamalia
+    def __init__(self, spesies):
+        self.__spesies = spesies
+    # Method tampilkan_spesies menampilkan sebuah pesan
+    # yang menunjukkan spesies dari mamalia.
+
+
+def tampilkan_spesies(self):
+    print('Saya adalah seekor', self.__spesies)
+    # Method buat_suara menampilkan suara generik dari mamalia
+
+
+def buat_suara(self):
+    print('Grrrr')
