310b47c5523a9f62d96a19671d45a657e849ce8b
hp <sahalnurdin@gmail.com>
2022-06-19 08:09:08 +0700

    SELESAI!!!

--- polymorphism.py
+++ polymorphism.py
@@ -66,4 +66,21 @@ bebek.tampilkan_spesies()
 bebek.buat_suara()
 
 
+# Soal Nomor 2
+class Sayuran:
+    def __init__(self, jenis_sayuran):
+        self.__jenis_sayuran = jenis_sayuran
+
+    def pesan(self):
+        print('Saya adalah sayuran.')
+
+class Kentang(Sayuran):
+    def __init__(self):
+        Sayuran.__init__(self, 'kentang')
+
+    def pesan(self):
+        print('Saya adalah kentang.')
+
+k = Kentang()
+k.pesan()
 
